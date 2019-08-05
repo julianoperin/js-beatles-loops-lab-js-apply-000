5b6b212942bccdde292c26cc06d2545fe8d1849b
Juliano Antonio Perin <julianoperins@gmail.com>
2019-08-05 19:20:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // add solution here
 
 var musicians = ["john Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-  var empty = [];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   
+  
+theBeatlesPlay(musicians, instruments);
 
 function theBeatlesPlay(musicians, instruments) {
-  
-  
-  
+  var result = [];
+  for (var i = 0; i < musicians.lenght; i++) {
+    result.push(musicians[i] + " plays" + instruments[i]);
+    }
 }
