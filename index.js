a1aaa3e49da496e1b49d6c98dd535a2955c5ce1a
Juliano Antonio Perin <julianoperins@gmail.com>
2019-08-05 19:32:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function theBeatlesPlay(musicians, instruments) {
   for(var i =0; i < musicians.length; i++) {
     newArr.push(musicians[i] + " plays " + instruments[i]);
   }
-  return thirdArraytArray;
+  return thirdArray;
 }
