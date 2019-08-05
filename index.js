c7e69143b984600931a3948cd32e164c7bf76208
Juliano Antonio Perin <julianoperins@gmail.com>
2019-08-05 19:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // add solution here
 
-function theBeatlesPlay(musicians, instruments) {
-  var musicians = ["john Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var musicians = ["john Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   var empty = [];
+  
+
+function theBeatlesPlay(musicians, instruments) {
   
   
   
